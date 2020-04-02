c212a7ba71fff1b558395107445791c3ab5e2ce4
Matthew Jones <matts3371@gmail.com>
2020-04-02 16:39:37 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,5 +25,9 @@ class ArticlesController < ApplicationController
   end 
   
   def update 
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], description: params[:description])
+    @article.save 
+    redirect_to article_path(@article)
   end 
 end
